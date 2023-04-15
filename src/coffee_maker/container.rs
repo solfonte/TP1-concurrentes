ab22e184636ac9e2c0da27b45ff0e5dc86ad327f
solfonte <msfontenla@fi.uba.ar>
2023-04-15 11:49:15 -0300

    feat: add extraction logic to container structure

--- src/coffee_maker/container.rs
+++ src/coffee_maker/container.rs
@@ -1,43 +1,70 @@
-use std::sync::{Arc, Condvar, Mutex};
+use std::{
+    fmt::Error,
+    sync::{Arc, Condvar, Mutex},
+};
 
 #[derive(Debug)]
 pub struct System {
     amount: u32,
     busy: bool,
-    is_on: bool
+    is_on: bool,
 }
-pub struct Container { /* cantidad actual, is_on, is_busy */
+pub struct Container {
+    /* cantidad actual, is_on, is_busy */
     pair: Arc<(Mutex<System>, Condvar)>,
+    name: String,
 }
 
 impl Container {
-    pub fn new(max_capacity: u32) -> Self {
+    pub fn new(max_capacity: u32, name: String) -> Self {
         Self {
-            pair: Arc::new((Mutex::new(System {amount: max_capacity, busy: false, is_on: true}), Condvar::new())),
+            pair: Arc::new((
+                Mutex::new(System {
+                    amount: max_capacity,
+                    busy: false,
+                    is_on: true,
+                }),
+                Condvar::new(),
+            )),
+            name,
         }
     }
 
-    pub fn extract(&self, extraction: u32) -> u32 {
-
-        let mut possible_extraction = 0;
+    pub fn extract(&self, extraction: u32) -> Result<u32, &str> {
+        let mut result = Ok(extraction);
         if let Ok(guard) = self.pair.0.lock() {
-            if let Ok(mut system) =  self.pair.1.wait_while(guard,|state| {
-                    println!("[Contenedor] Chequeando {:?}", state);
-                    state.busy
-            }){
+            if let Ok(mut system) = self.pair.1.wait_while(guard, |state| {
+                (state.busy && state.is_on)
+                    || (!state.busy && state.is_on && state.amount < extraction)
+            }) {
+                println!("[container {}] {:?}", self.name, *system);
+                
                 (*system).busy = true;
 
-                if (*system).amount >= extraction {
+                if !(*system).is_on {
+                    result = Ok(0);
+                } else {
                     (*system).amount -= extraction;
-                    possible_extraction = extraction;
-                }else {
+
+                }
+
+                if (*system).amount == 0 {
                     (*system).is_on = false;
                 }
+
                 (*system).busy = false;
                 self.pair.1.notify_all();
             }
         }
-        possible_extraction 
+        result
     }
-    
 }
+
+/*
+Condiciones
+    -> busy && on -> espero
+    -> !busy && on && !amount -> espero (por lo menos hasta que se apague)
+    -> !on -> NO espero. Se apago y tendria que devolver false
+    -> !busy && on && amount -> No espero. accedo
+
+*/
